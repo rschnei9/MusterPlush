9c876ceb252bf70b1569c8f0e137f73836d8b493
rschnei9 <rschnei9@student.fitchburgstate.edu>
2026-04-08 09:48:32 -0400

    aaaaaaaaaaaadd
    something sprites

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public float Defense;
+    public float Power;
+    public float Speed;
+    public float Buff;
+    public float Debuff;
+    public float Condition;
+
+    public float DebuffTimer;
+    public float BuffTimer;
+    public float ConditionTimer;
+
+    public float Player;
+    public float Enemy;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
