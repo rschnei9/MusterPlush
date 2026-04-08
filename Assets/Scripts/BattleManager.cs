97a9d391612b12bcefad27e72293b1561cffd1e6
rschnei9 <rschnei9@student.fitchburgstate.edu>
2026-04-08 11:01:30 -0400

    sprite script changes
    sandwich

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
+//THESE VARIABLES ARE FOR THE PLAYER
+public GameObject [] Plushes;
+    public float Player;
+
     public float Defense;
     public float Power;
     public float Speed;
+
     public float Buff;
     public float Debuff;
     public float Condition;
@@ -13,8 +18,42 @@ public class BattleManager : MonoBehaviour
     public float BuffTimer;
     public float ConditionTimer;
 
-    public float Player;
+//THESE VARIABLES ARE FOR THE ENEMY
+public GameObject [] Enemies;
     public float Enemy;
+
+    public float EDefense;
+    public float EPower;
+    public float ESpeed;
+
+    public float EBuff;
+    public float EDebuff;
+    public float ECondition;
+
+    public float EDebuffTimer;
+    public float EBuffTimer;
+    public float EConditionTimer;
+
+//THESE ARE FOR THE UI
+    public GameObject [] Buttons;
+    public bool Select;
+
+    public float ButtonA;
+    public float ButtonB;
+    public float ButtonC;
+    public float ButtonD;
+
+    public float MoveA;
+    public float MoveB;
+    public float MoveC;
+    public float MoveD;
+    public float MoveE;
+    public float MoveF;
+    public float MoveG;
+    public float MoveH;
+    public float MoveI;
+    public float MoveJ;
+
     void Start()
     {
         
